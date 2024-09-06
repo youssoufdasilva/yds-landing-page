23b5b99933e5422575c8c84898e1c70b5bab0fb5
Youssouf da Silva <youssouf@intellium.ai>
2024-09-06 14:05:55 +0000

    updated the content of the website

--- components/resume-card.tsx
+++ components/resume-card.tsx
@@ -5,9 +5,10 @@ import { Badge } from "@/components/ui/badge";
 import { Card, CardHeader } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
-import { ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon, LinkIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 interface ResumeCardProps {
   logoUrl: string;
@@ -31,80 +32,83 @@ export const ResumeCard = ({
 }: ResumeCardProps) => {
   const [isExpanded, setIsExpanded] = React.useState(false);
 
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+  const handleClick = () => {
     if (description) {
-      e.preventDefault();
       setIsExpanded(!isExpanded);
     }
   };
 
   return (
-    <Link
-      href={href || "#"}
-      className="block cursor-pointer"
-      onClick={handleClick}
-    >
-      <Card className="flex">
-        <div className="flex-none">
-          <Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
-            <AvatarImage
-              src={logoUrl}
-              alt={altText}
-              className="object-contain"
-            />
-            <AvatarFallback>{altText[0]}</AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="flex-grow ml-4 items-center flex-col group">
-          <CardHeader>
-            <div className="flex items-center justify-between gap-x-2 text-base">
-              <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
-                {title}
-                {badges && (
-                  <span className="inline-flex gap-x-1">
-                    {badges.map((badge, index) => (
-                      <Badge
-                        variant="secondary"
-                        className="align-middle text-xs"
-                        key={index}
-                      >
-                        {badge}
-                      </Badge>
-                    ))}
-                  </span>
+    <Card onClick={handleClick} className="flex cursor-pointer">
+      <div className="flex-none">
+        <Avatar className="border size-12 m-auto bg-muted-background dark:bg-foreground">
+          <AvatarImage src={logoUrl} alt={altText} className="object-contain" />
+          <AvatarFallback>{altText[0]}</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex-grow ml-4 items-center flex-col group">
+        <CardHeader>
+          <div className="flex items-center justify-between gap-x-2 text-base">
+            <h3 className="inline-flex items-center justify-center font-semibold leading-none text-xs sm:text-sm">
+              {title}
+              {badges && (
+                <span className="inline-flex gap-x-1">
+                  {badges.map((badge, index) => (
+                    <Badge
+                      variant="secondary"
+                      className="align-middle text-xs"
+                      key={index}
+                    >
+                      {badge}
+                    </Badge>
+                  ))}
+                </span>
+              )}
+              <ChevronRightIcon
+                className={cn(
+                  "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
+                  isExpanded ? "rotate-90" : "rotate-0"
                 )}
-                <ChevronRightIcon
-                  className={cn(
-                    "size-4 translate-x-0 transform opacity-0 transition-all duration-300 ease-out group-hover:translate-x-1 group-hover:opacity-100",
-                    isExpanded ? "rotate-90" : "rotate-0"
-                  )}
-                />
-              </h3>
-              <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
-                {period}
-              </div>
+              />
+            </h3>
+            <div className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
+              {period}
             </div>
-            {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
-          </CardHeader>
-          {description && (
-            <motion.div
-              initial={{ opacity: 0, height: 0 }}
-              animate={{
-                opacity: isExpanded ? 1 : 0,
+          </div>
+          {subtitle && <div className="font-sans text-xs">{subtitle}</div>}
+        </CardHeader>
+        {description && (
+          <motion.div
+            initial={{ opacity: 0, height: 0 }}
+            animate={{
+              opacity: isExpanded ? 1 : 0,
 
-                height: isExpanded ? "auto" : 0,
-              }}
-              transition={{
-                duration: 0.7,
-                ease: [0.16, 1, 0.3, 1],
-              }}
-              className="mt-2 text-xs sm:text-sm"
-            >
-              {description}
-            </motion.div>
-          )}
-        </div>
-      </Card>
-    </Link>
+              height: isExpanded ? "auto" : 0,
+            }}
+            transition={{
+              duration: 0.7,
+              ease: [0.16, 1, 0.3, 1],
+            }}
+            className="mt-2 text-xs sm:text-sm"
+          >
+            <span>{description}</span>
+            {href && (
+              <div className="mt-2">
+                <Link href={href} target="_blank" rel="noopener noreferrer">
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="text-xs flex== items-center gap-1"
+                  >
+                    <LinkIcon className="ml-2 h-3 w-3" />
+                    Visit
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </motion.div>
+        )}
+      </div>
+    </Card>
   );
 };
--- data/resume.tsx
+++ data/resume.tsx
@@ -8,8 +8,7 @@ export const DATA = {
   url: "https://youssoufdasilva.com",
   location: "Bristol, UK",
   locationLink: "https://www.google.com/maps/place/Bristol",
-  description:
-    "Senior Full Stack Software Engineer | Project Management",
+  description: "Senior Full Stack Software Engineer | Project Management",
   summary:
     "A dedicated FullStack Software Engineer with a keen eye for detail and a commitment to crafting efficient, user-centric web experiences. I thrive on tackling complex challenges, leveraging my expertise in front-end and back-end technologies to deliver robust, scalable solutions that exceed expectations and delight users.",
   avatarUrl: "/me.jpeg",
@@ -73,26 +72,85 @@ export const DATA = {
       company: "Intellium AI",
       href: "https://intellium.ai",
       badges: [],
-      location: "Remote",
+      location: "Bristol, England, United Kingdom · On-site",
       title: "Senior Full Stack Software Engineer",
-      logoUrl: "/intellium.svg",
-      start: "July 2023",
+      logoUrl: "/intellium.jpeg",
+      start: "Jul 2023",
       end: "Present",
-      description:
-        "wip - Adipisicing sint laboris mollit aute pariatur fugiat excepteur do nulla. Commodo ea sit sunt exercitation quis consectetur deserunt fugiat. Amet sint aute qui ex ipsum incididunt dolore ullamco. Tempor ipsum adipisicing ullamco ullamco ipsum nisi magna officia duis deserunt ex. Officia aliquip amet pariatur aute aliqua deserunt aliquip magna irure.",
+      description: `Key Job Responsibilities:
+- Develop attractive and intuitive dashboards with React, NodeJs, JavaScript, HTML, CSS to build, deploy and scale Enterprise AI software
+- Develop code in python and UNIX scripts to build, deploy and scale Enterprise AI software 
+- Work in backend tech stack involving flask, NGINX, docker, node etc. 
+- Develop AI code to add new features to the Enterprise AI platform
+- Develop unit test, integration test for CI/CD 
+- Write code documentation following software engineering best practices`,
+    },
+    {
+      company: "Fox Robotics",
+      href: "https://fox-robotics.com/",
+      badges: [],
+      location: "Surrey, England, United Kingdom · On-site",
+      title: "Software Engineer",
+      logoUrl: "/foxrobotics.jpeg",
+      start: "Jul 2020",
+      end: "Jun 2023",
+      description: `I was responsible for developing the robotics-supporting web application's backend and frontend development and supported the desig  and development of other core components nodes for the robot.`,
+    },
+    {
+      company: "Akatuapa Limited (PsypherTxt)",
+      href: "https://fox-robotics.com/",
+      badges: [],
+      location: "Surrey, England, United Kingdom · On-site",
+      title: "Web / Backend Software Developer",
+      logoUrl: "/akatuapa.jpeg",
+      start: "May 2017",
+      end: "Aug 2019",
+      description: `Worked in a small team of 10 people to create mobile apps and games. Crafted beautiful websites and supported interns within the team.
+
+- Developed a web application (gstickerss.com) using React JS to showcase company products.
+- Created a responsive email template using Foundation Emails for newsletters.
+- Developed a mobile game (Goldbar) using Unity.
+- Built two websites to promote Goldbar using React JS.
+- Designed a web application using Adobe XD for a Beta Testing platform.
+- Directed and edited short informational videos using iMovie to raise awareness for company products.
+- Converted Bonanza, a hard copy card game into a web game.
+- Mentored new starters and interns throughout their employment 
+- Conducted interviews and created interview transcripts for the panel.`,
+    },
+    {
+      company: "PsypherTxt",
+      // href: "https://fox-robotics.com/",
+      badges: [],
+      location: "Greater Accra Region, Ghana · On-site",
+      title: "Software Intern",
+      logoUrl: "/psyphertxt.jpeg",
+      start: "Jun 2016",
+      end: "Aug 2016",
+      description: `Psyphertxt is made up of a passionate team developing mobile apps and games committed to a mission of fostering innovation through our varied mobile app products.
+
+- Designed and developed an android based mobile application for online auctions 
+- Marketed a messaging application that the company was building at the 2016 Africa Mobile & ICT Expo`,
     },
   ],
   education: [
     {
+      school: "Middlesex University",
+      href: "https://ashesi.edu.gh",
+      degree: "MSc BISM",
+      logoUrl: "/middlesex.png",
+      start: "2019",
+      end: "2020",
+      description: "Masters in Business Information Systems Management (BISM)",
+    },
+    {
       school: "Ashesi University",
       href: "https://ashesi.edu.gh",
       degree: "BSc Computer Science",
-      logoUrl: "/Ashesi.webp",
+      logoUrl: "/ashesi.jpeg",
       start: "2013",
       end: "2017",
-      description:
-        "wip - Adipisicing sint laboris mollit aute pariatur fugiat excepteur do nulla. Commodo ea sit sunt exercitation quis consectetur deserunt fugiat. Amet sint aute qui ex ipsum incididunt dolore ullamco. Tempor ipsum adipisicing ullamco ullamco ipsum nisi magna officia duis deserunt ex. Officia aliquip amet pariatur aute aliqua deserunt aliquip magna irure.",
+      description: "Bachelor in Computer Science",
     },
   ],
-  projects: []
+  projects: [],
 } as const;
