26bc5593dfaad0d3de5162eb6f1a6afdf24ced03
Youssouf da Silva <youssoufdasilva@gmail.com>
2024-12-18 19:48:58 +0000

    removing the export config to allow the app to work

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/yds-landing-page",
-  output: "export",
-  reactStrictMode: true,
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
